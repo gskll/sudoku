a09b81e643e0b321223ceee54cda206dbd514a87
Andrew <gskll@protonmail.com>
2021-01-05 23:13:09 +0100

    feat: improve styling with chakra-ui of sudoku board

--- src/components/SudokuField.js
+++ src/components/SudokuField.js
@@ -1,14 +1,25 @@
 import { Input } from '@chakra-ui/react';
 import React from 'react';
 
-const SudokuField = ({ field }) => (
-  <Input
-    isReadOnly={field.readonly}
-    focusBorderColor={field.readonly ? 'crimson.400' : 'teal.400'}
-    value={field.value}
-    boxSize="50px"
-    m="5px"
-  />
-);
+const SudokuField = ({ field }) => {
+  const inputProps = {
+    boxSize: '50px',
+    margin: '5px',
+    textAlign: 'center',
+    size: 'lg',
+  };
+
+  if (field.readonly) {
+    inputProps.isReadOnly = true;
+    inputProps.isDisabled = true;
+    inputProps.opacity = '100% !important';
+    inputProps.cursor = 'default !important';
+    inputProps.variant = 'filled';
+    inputProps.value = field.value + 1;
+    inputProps.fontWeight = 'bold';
+  }
+
+  return <Input {...inputProps} />;
+};
 
 export default SudokuField;
