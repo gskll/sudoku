0ececa96e79f18a02a24e92a1ad23ca6a7972319
Andrew <gskll@protonmail.com>
2021-01-06 13:40:56 +0100

    feat: display SudokuBoard with CSS Grid
    - Use Chakra Grid component to set up 9x9 grid
    - Map over new board array
    -   Display Flex comp and inner SudokuField for each
    -   If relevant, display borders on Flex
    -      for inner 3x3 separation
    - SudokuField:
    -   if field is readonly -> display in Flex
    -   else display as input
    -   both field types styled similarly

--- src/components/SudokuBoard.js
+++ src/components/SudokuBoard.js
@@ -1,30 +1,41 @@
 import React from 'react';
-import { Box, Button } from '@chakra-ui/react';
-import { DownloadIcon } from '@chakra-ui/icons';
+import { Flex, Grid } from '@chakra-ui/react';
 
 import SudokuField from './SudokuField';
 
+// TODO: control elements to ensure numbers
+
 const SudokuBoard = ({ sudoku }) => {
+  console.log(sudoku);
   return (
-    <>
-      <Box>
-        {sudoku.rows.map(row => (
-          <Box key={row.index}>
-            {row.cols.map(field => (
-              <SudokuField field={field} key={field.col} />
-            ))}
-          </Box>
-        ))}
-      </Box>
-      <Button
-        leftIcon={<DownloadIcon />}
-        colorScheme="blue"
-        variant="solid"
-        m="3vh auto"
-      >
-        Show Solution
-      </Button>
-    </>
+    <Grid
+      boxSize={{ sm: '80vw', lg: '80vh' }}
+      templateColumns="repeat(9, 1fr)"
+      templateRows="repeat(9, 1fr)"
+      m={15}
+    >
+      {sudoku.map(el => {
+        const border = { border: null };
+
+        if (el.col === 2 || el.col === 5) {
+          border.borderRightColor = 'gray.300';
+          border.borderRightStyle = 'solid';
+          border.borderRightWidth = '.5vh';
+        }
+
+        if (el.row === 2 || el.row === 5) {
+          border.borderBottomColor = 'gray.300';
+          border.borderBottomStyle = 'solid';
+          border.borderBottomWidth = '.5vh';
+        }
+
+        return (
+          <Flex key={el.index} align="center" justify="center" {...border}>
+            <SudokuField field={el} key={el.index * 2} />
+          </Flex>
+        );
+      })}
+    </Grid>
   );
 };
 
--- src/components/SudokuField.js
+++ src/components/SudokuField.js
@@ -1,35 +1,24 @@
-import { Input } from '@chakra-ui/react';
+import { Flex, Input } from '@chakra-ui/react';
 import React from 'react';
 
 const SudokuField = ({ field }) => {
-  const inputProps = {
-    boxSize: '50px',
-    margin: '5px',
-    textAlign: 'center',
-    size: 'lg',
-  };
-
   if (field.readonly) {
-    inputProps.isReadOnly = true;
-    inputProps.isDisabled = true;
-    inputProps.opacity = '100% !important';
-    inputProps.cursor = 'default !important';
-    inputProps.variant = 'filled';
-    inputProps.value = field.value + 1;
-    inputProps.fontWeight = 'bold';
+    return (
+      <Flex
+        boxSize="90%"
+        align="center"
+        justify="center"
+        bg="gray.50"
+        fontWeight="bold"
+        borderColor="gray.300"
+        borderRadius="0.375em"
+      >
+        {' '}
+        {field.value + 1}
+      </Flex>
+    );
+  } else {
+    return <Input boxSize="90%" textAlign="center" borderColor="gray.300" />;
   }
-
-  if (field.row === 2 || field.row === 5) {
-    inputProps.borderBottomColor = 'gray.600';
-    inputProps.borderBottomWidth = '5px';
-  }
-
-  if (field.col === 2 || field.col === 5) {
-    inputProps.borderRightColor = 'gray.600';
-    inputProps.borderRightWidth = '5px';
-  }
-
-  return <Input {...inputProps} />;
 };
-
 export default SudokuField;
