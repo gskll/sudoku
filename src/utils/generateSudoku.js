343cb092a43caa0588fae6df766417ab2de9139f
Andrew <gskll@protonmail.com>
2021-01-06 01:00:46 +0100

    refactor: generateSudoku util to store solutions in original board

--- src/utils/generateSudoku.js
+++ src/utils/generateSudoku.js
@@ -1,20 +1,39 @@
-import { makepuzzle } from 'sudoku';
-import convertSudokuToBoard from './convertSudokuToBoard';
+import { makepuzzle, solvepuzzle } from 'sudoku';
 
 /*
  * Generates a sudoku board with structure:
- * {rows: [{index: 0, cols: [{row: 0, col: 0, value: 1, readonly: true}, ...]}, ...]}
+ * {rows: [{index: 0, cols: [{row: 0, col: 0, value: 1, readonly: true, solution: 1}, ...]}, ...]}
  */
 
 const generateSudoku = () => {
-  const rawSudoku = makepuzzle();
+  const raw = makepuzzle();
+  const solved = solvepuzzle(raw);
 
-  const board = convertSudokuToBoard(rawSudoku);
+  const board = { rows: [] };
 
-  return {
-    rawSudoku,
-    board,
-  };
+  for (let i = 0; i < 9; i++) {
+    const row = { index: i, cols: [] };
+
+    for (let j = 0; j < 9; j++) {
+      const index = i * 9 + j;
+      const value = raw[index];
+      const solution = solved[index];
+
+      const col = {
+        row: i,
+        col: j,
+        value,
+        solution,
+        readonly: value !== null,
+      };
+
+      row.cols.push(col);
+    }
+
+    board.rows.push(row);
+  }
+
+  return board;
 };
 
 export default generateSudoku;
