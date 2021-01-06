4b67b85e7b8953124210911eef47afaaaaac55c5
Andrew <gskll@protonmail.com>
2021-01-06 13:37:12 +0100

    refactor: changes to display board with CSS Grid
    - change default sudoku state to empty array
    - move solution button to App component from Board
    - set App Flex container as height=100vh to prevent overflow

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
-import { ChakraProvider, Flex, Heading } from '@chakra-ui/react';
+import { Button, ChakraProvider, Flex, Heading } from '@chakra-ui/react';
 
 import generateSudoku from '../utils/generateSudoku';
 
 import SudokuBoard from './SudokuBoard';
+import { DownloadIcon } from '@chakra-ui/icons';
 
 const App = () => {
-  const [sudoku, setSudoku] = useState({ rows: [] });
+  const [sudoku, setSudoku] = useState([]);
 
   useEffect(() => {
     setSudoku(generateSudoku());
@@ -20,11 +21,20 @@ const App = () => {
         align="center"
         maxW={{ xl: '1200px' }}
         m="0 auto"
+        h="100vh"
       >
         <Heading as="h1" m="3vh auto">
           Sweeeeeet Sudoku
         </Heading>
         <SudokuBoard sudoku={sudoku} />
+        <Button
+          leftIcon={<DownloadIcon />}
+          colorScheme="blue"
+          variant="solid"
+          m="3vh auto"
+        >
+          Show Solution
+        </Button>
       </Flex>
     </ChakraProvider>
   );
