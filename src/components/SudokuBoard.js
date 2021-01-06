4c4760a194ba3cb06ed6916236e625dfb9f90a59
Andrew <gskll@protonmail.com>
2021-01-06 01:01:13 +0100

    feat: add button to show board solution

--- src/components/SudokuBoard.js
+++ src/components/SudokuBoard.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
+import { DownloadIcon } from '@chakra-ui/icons';
+
 import SudokuField from './SudokuField';
 
 const SudokuBoard = ({ sudoku }) => {
   return (
-    <Box>
-      {sudoku.rows.map(row => (
-        <Box key={row.index}>
-          {row.cols.map(field => (
-            <SudokuField field={field} key={field.col} />
-          ))}
-        </Box>
-      ))}
-    </Box>
+    <>
+      <Box>
+        {sudoku.rows.map(row => (
+          <Box key={row.index}>
+            {row.cols.map(field => (
+              <SudokuField field={field} key={field.col} />
+            ))}
+          </Box>
+        ))}
+      </Box>
+      <Button
+        leftIcon={<DownloadIcon />}
+        colorScheme="blue"
+        variant="solid"
+        m="3vh auto"
+      >
+        Show Solution
+      </Button>
+    </>
   );
 };
 
