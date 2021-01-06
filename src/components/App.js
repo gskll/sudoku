39bc3c45c81961911a0148c62e843d09c578c3ea
Andrew <gskll@protonmail.com>
2021-01-06 01:00:11 +0100

    refactor: store board solution in original board, remove utils

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,16 @@
 import React, { useState, useEffect } from 'react';
+
 import { ChakraProvider, Flex, Heading } from '@chakra-ui/react';
 
 import generateSudoku from '../utils/generateSudoku';
-import solveSudoku from '../utils/solveSudoku';
 
 import SudokuBoard from './SudokuBoard';
 
 const App = () => {
   const [sudoku, setSudoku] = useState({ rows: [] });
-  const [solvedSudoku, setSolvedSudoku] = useState({ rows: [] });
 
   useEffect(() => {
-    const { rawSudoku, board } = generateSudoku();
-    setSudoku(board);
-    const { solvedBoard } = solveSudoku(rawSudoku);
-    setSolvedSudoku(solvedBoard);
+    setSudoku(generateSudoku());
   }, []);
 
   return (
@@ -26,7 +22,7 @@ const App = () => {
         m="0 auto"
       >
         <Heading as="h1" m="3vh auto">
-          Sweet Sudoku
+          Sweeeeeet Sudoku
         </Heading>
         <SudokuBoard sudoku={sudoku} />
       </Flex>
